be143c5ecaf4030e90f0696a07d4f03af8670d90
tvzgomes <thiago@poloplanejamento.com>
2021-08-02 15:49:18 -0300

    inicio aula 5

---
+++ scripts/aula5.R
@@ -0,0 +1,15 @@
+###Prática no R! Roteiro 4 - Correlação 
+# https://luisfelipebr.github.io/mti2020/roteiros/roteiro5.html
+
+#1. introdução
+
+install.packages("tidyverse")
+install.packages("performance")
+install.packages("broom")
+
+library(tidyverse)
+library(performance)
+library(broom)
+
+#2. regressão linear simples
+
